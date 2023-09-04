49d78ee9659332ca90ad3a62e96b2a7f41c5aacc
Kimi <KimiNako@users.noreply.github.com>
2023-09-05 00:00:04 +0200

    cleanup: remove cv test

--- cv/src/components/HelloWorld.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="hello"/>
-</template>
-
-<style scoped>
-</style>
--- cv/src/plugins/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { createI18n } from 'vue-i18n'
-import en from '@/locales/en.json'
-import fr from '@/locales/fr.json'
-
-export default createI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'fr',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'fr',
-  messages: {en, fr}
-})
